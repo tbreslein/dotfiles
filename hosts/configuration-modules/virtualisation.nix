544d21a944634ec5f87037fc67076da4caef1d8c
tbreslein <tommy.breslein@protonmail.com>
2022-08-01 14:59:23 +0200

    add docker stuff

--- hosts/configuration-modules/virtualisation.nix
+++ hosts/configuration-modules/virtualisation.nix
@@ -1,8 +1,16 @@
 { config, pkgs, ... }:
 
 {
+  # virt manager
   virtualisation.libvirtd.enable = true;
   programs.dconf.enable = true;
   environment.systemPackages = [ pkgs.virt-manager ];
   users.users.tommy.extraGroups = [ "libvirtd" ];
+
+  # docker
+  virtualisation.docker = {
+    rootless.enable = true;
+    autoPrune.enable = true;
+  };
+  # users.users.tommy.extraGroups = [ "docker" ];
 }
