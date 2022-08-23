8f1df2f36857a9b36dcad8483a7200a0829031d1
tbreslein <tommy.breslein@protonmail.com>
2022-08-23 13:04:05 +0200

    I have not given up on fennel yet...

---
+++ configs/nvim-fnl/init.lua
@@ -0,0 +1,18 @@
+local hotpot_path = vim.fn.stdpath('data') ..
+                        '/site/pack/paqs/start/hotpot.nvim'
+
+if vim.fn.empty(vim.fn.glob(hotpot_path)) > 0 then
+    print("Could not find hotpot.nvim, cloning new copy to", hotpot_path)
+    vim.fn.system({
+        'git', 'clone', 'https://github.com/rktjmp/hotpot.nvim', hotpot_path
+    })
+    vim.cmd("helptags " .. hotpot_path .. "/doc")
+end
+
+-- Enable fnl/ support
+require("hotpot")
+
+-- Now you can load fennel code, so you could put the rest of your
+-- config in a separate `~/.config/nvim/fnl/my_config.fnl` or
+-- `~/.config/nvim/fnl/plugins.fnl`, etc.
+require("config")
