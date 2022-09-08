f46c8d792766ea3e0ae839ed31738b32037df37a
tbreslein <tommy.breslein@protonmail.com>
2022-09-08 15:30:59 +0200

    chore: formatting

--- configs/nvim-fnl/plugin/0-bootstraps.lua
+++ configs/nvim-fnl/plugin/0-bootstraps.lua
@@ -4,12 +4,12 @@ local pack = "packer"
 local function bootstrap(url)
     local name = url:gsub(".*/", "")
     local path =
-        vim.fn.stdpath [[data]] .. "/site/pack/" .. pack .. "/start/" .. name
+    vim.fn.stdpath [[data]] .. "/site/pack/" .. pack .. "/start/" .. name
 
     if vim.fn.isdirectory(path) == 0 then
         print(name .. ": installing in data dir...")
 
-        vim.fn.system {"git", "clone", "--depth", "1", url, path}
+        vim.fn.system { "git", "clone", "--depth", "1", url, path }
 
         vim.cmd [[redraw]]
         print(name .. ": finished installing")
@@ -19,9 +19,8 @@ end
 bootstrap "https://github.com/udayvir-singh/tangerine.nvim"
 bootstrap "https://github.com/udayvir-singh/hibiscus.nvim"
 
--- Enable fnl/ support
-require[[tangerine]].setup {
+require [[tangerine]].setup {
     target = vim.fn.stdpath [[data]] .. "/tangerine",
-    compiler = {hooks = {"oninit"}},
-    eval = {luafmt = function() return {"lua-format"} end}
+    compiler = { hooks = { "oninit" } },
+    eval = { luafmt = function() return { "lua-format" } end }
 }
