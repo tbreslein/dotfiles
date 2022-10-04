6c3a60a9543a6300f6d45303c876b22b8d842714
tbreslein <tommy.breslein@protonmail.com>
2022-10-04 08:44:39 +0200

    fix(nixos): backup-to-styx should now work as intended

--- hosts/audron/default.nix
+++ hosts/audron/default.nix
@@ -29,7 +29,7 @@ in
   environment = {
     systemPackages = with pkgs; [
       (writeShellScriptBin "backup-to-styx" ''
-        rsync -a --info=progress2 ${homeDir}/work root@styx:/archive/admin/audron/$(date +$F)/work
+        rsync -a --info=progress2 ${homeDir}/work ganymedroot:/archive/admin/audron/$(date +$F)/
       '')
 
       # needed for eduroam:
