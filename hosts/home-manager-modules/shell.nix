b023321566cffa5049fbb669e6e0dc76b5063889
tbreslein <tommy.breslein@protonmail.com>
2022-12-03 18:15:40 +0100

    move nix shell symbol to first line in prompt

--- hosts/home-manager-modules/shell.nix
+++ hosts/home-manager-modules/shell.nix
@@ -73,7 +73,7 @@
           truncate_to_repo = false;
           style = "bold blue";
         };
-        format = "$directory\\[ $git_branch$git_commit$git_state$git_metrics$git_status\\]$line_break$time$nix_shell$character";
+        format = "$directory\\[ $git_branch$git_commit$git_state$git_metrics$git_status\\]$nix_shell$line_break$time$character";
         git_branch.format = "[$branch(:$remote_branch)]($style) ";
         nix_shell.format = "[$symbol]($style)";
         time = {
